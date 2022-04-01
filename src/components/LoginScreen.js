91a7f232d9a2fbecd1f3bda49d2343e5a0c9b670
nubol23 <rafael.rvp98@gmail.com>
2022-04-01 19:07:58 -0400

    handle login text

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import {useForm} from "../hooks/useForm";
 
 const LoginScreen = () => {
+
+  const [{email, password}, handleInputChange] = useForm({
+    email: "",
+    password: "",
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (email === "" || password === "") return;
+
+    console.log(email)
+    console.log(password)
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           type="text"
           placeholder="Email"
           name="email"
+          value={email}
+          onChange={handleInputChange}
         />
 
         <input
           type="password"
           placeholder="Password"
           name="password"
+          value={password}
+          onChange={handleInputChange}
         />
 
         <button type="submit">
