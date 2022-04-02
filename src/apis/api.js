c73c513f112e103d5e7a4dfdfe7d966ee8c007b7
nubol23 <rafael.rvp98@gmail.com>
2022-04-01 22:08:26 -0400

    wrapped get request method

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,13 +1,51 @@
+// const api = setUpInterceptorsTo(
+//   axios.create({
+//     baseURL: "http://127.0.0.1:8000/",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   })
+// );
+
 import {setUpInterceptorsTo} from "./interceptors";
 import axios from "axios";
 
-const api = setUpInterceptorsTo(
-  axios.create({
-    baseURL: "http://127.0.0.1:8000/",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-);
+class Api {
+  constructor() {
+    this.axiosInstance = setUpInterceptorsTo(
+      axios.create({
+        baseURL: "http://127.0.0.1:8000/",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    );
+  }
+
+  get(path) {
+    return new Promise((resolve, reject) => {
+      this.axiosInstance.get(path)
+        .then((response) => {
+          if (response) {
+            resolve(response);
+          }
+          reject();
+        })
+        .catch((error) => {
+          // Retry request if returned 401 as the token was refreshed
+          if (error.response.status === 401) {
+            console.log("Retried request")
+            resolve(this.axiosInstance.get(path)
+              .then((response) => response)
+              .catch((error) => error))
+          }
+
+          reject(error);
+        })
+    });
+  }
+}
+
+const api = new Api();
 
 export default api;
