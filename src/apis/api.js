ba5e9f92b609d4529c1edbc41bd9f9ee48abdbdd
nubol23 <rafael.rvp98@gmail.com>
2022-04-01 23:33:26 -0400

    added other http methods

--- src/apis/api.js
+++ src/apis/api.js
@@ -34,7 +34,102 @@ class Api {
           if (error.response.status === 401) {
             resolve(this.axiosInstance.get(path)
               .then((response) => response)
-              .catch((error) => error))
+              .catch((error) => {
+                localStorage.setItem("user", JSON.stringify({logged: false}))
+                return error
+              }))
+          }
+
+          reject(error);
+        })
+    });
+  }
+
+  post(path) {
+    return new Promise((resolve, reject) => {
+      this.axiosInstance.post(path)
+        .then((response) => {
+          if (response) resolve(response);
+          reject();
+        })
+        .catch((error) => {
+          // Retry request if returned 401 as the token was refreshed
+          if (error.response.status === 401) {
+            resolve(this.axiosInstance.post(path)
+              .then((response) => response)
+              .catch((error) => {
+                localStorage.setItem("user", JSON.stringify({logged: false}))
+                return error
+              }))
+          }
+
+          reject(error);
+        })
+    });
+  }
+
+  put(path) {
+    return new Promise((resolve, reject) => {
+      this.axiosInstance.put(path)
+        .then((response) => {
+          if (response) resolve(response);
+          reject();
+        })
+        .catch((error) => {
+          // Retry request if returned 401 as the token was refreshed
+          if (error.response.status === 401) {
+            resolve(this.axiosInstance.put(path)
+              .then((response) => response)
+              .catch((error) => {
+                localStorage.setItem("user", JSON.stringify({logged: false}))
+                return error
+              }))
+          }
+
+          reject(error);
+        })
+    });
+  }
+
+  patch(path) {
+    return new Promise((resolve, reject) => {
+      this.axiosInstance.patch(path)
+        .then((response) => {
+          if (response) resolve(response);
+          reject();
+        })
+        .catch((error) => {
+          // Retry request if returned 401 as the token was refreshed
+          if (error.response.status === 401) {
+            resolve(this.axiosInstance.patch(path)
+              .then((response) => response)
+              .catch((error) => {
+                localStorage.setItem("user", JSON.stringify({logged: false}))
+                return error
+              }))
+          }
+
+          reject(error);
+        })
+    });
+  }
+
+  delete(path) {
+    return new Promise((resolve, reject) => {
+      this.axiosInstance.delete(path)
+        .then((response) => {
+          if (response) resolve(response);
+          reject();
+        })
+        .catch((error) => {
+          // Retry request if returned 401 as the token was refreshed
+          if (error.response.status === 401) {
+            resolve(this.axiosInstance.delete(path)
+              .then((response) => response)
+              .catch((error) => {
+                localStorage.setItem("user", JSON.stringify({logged: false}))
+                return error
+              }))
           }
 
           reject(error);
