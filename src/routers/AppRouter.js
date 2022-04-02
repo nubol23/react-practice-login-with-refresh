44457a81e14cc09e1b6b865210d172d3c7d1a83d
nubol23 <rafael.rvp98@gmail.com>
2022-04-01 20:37:47 -0400

    added private and public protector routers

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,14 +2,24 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import LoginScreen from "../components/LoginScreen";
 import DashboardRouter from "./DashboardRouter";
+import PrivateRouter from "./PrivateRouter";
+import PublicRouter from "./PublicRouter";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginScreen/>}/>
+        <Route path="/login" element={
+          <PublicRouter>
+            <LoginScreen/>
+          </PublicRouter>
+        }/>
 
-        <Route path="/*" element={<DashboardRouter/>}/>
+        <Route path="/*" element={
+          <PrivateRouter>
+            <DashboardRouter/>
+          </PrivateRouter>
+        }/>
       </Routes>
     </BrowserRouter>
   );
